a432a79fdd3df43ef368aaee08955da5e786e158
somadisingh <btech10001.19@bitmesra.ac.in>
2022-03-17 10:04:32 +0530

    passed objects as props which reduced the code

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ import cardData from "./components/Data";
 
 export default function App() {
   const cardElements = cardData.map((card) => {
-    return (
-      <Card
-        key={card.id}
-        coverImg={card.coverImg}
-        rating={card.stats.rating}
-        reviewCount={card.stats.reviewCount}
-        location={card.location}
-        title={card.title}
-        price={card.price}
-        openSpots={card.openSpots}
-      />
-    );
+    return <Card key={card.id} card={card} />;
   });
 
   return (
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,32 +2,32 @@ import React from "react";
 
 export default function Card(props) {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.card.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.location === "Online") {
+  } else if (props.card.location === "Online") {
     badgeText = "ONLINE";
   }
   return (
     <div className="card">
       {badgeText && <div className="status">{badgeText}</div>}
-      <img src={`images/${props.coverImg}`} className="swimmer" alt=" " />
+      <img src={`images/${props.card.coverImg}`} className="swimmer" alt=" " />
 
       <div className="info">
         <img src={require("../images/star.png")} className="star" alt="" />
-        <span>{props.rating}</span>
+        <span>{props.card.rating}</span>
         <span className="gray">
-          ({props.reviewCount}) • {props.location}
+          ({props.card.reviewCount}) • {props.card.location}
         </span>
       </div>
 
       <div className="info">
         <div>
-          <p>{props.title}</p>
+          <p>{props.card.title}</p>
         </div>
 
         <div>
           <span>
-            <b>From ${props.price}</b>
+            <b>From ${props.card.price}</b>
           </span>
           <span>/ person</span>
         </div>
